c7b6c305a8d164bba12b8e6b95d4b2e31f657df5
somnath46 <4684.somnath@gmail.com>
2021-07-06 19:08:33 +0530

    Create configuration server to fetch the common properties #1

---
+++ src/test/java/com/jt/cloud/config/JtCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jt.cloud.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
